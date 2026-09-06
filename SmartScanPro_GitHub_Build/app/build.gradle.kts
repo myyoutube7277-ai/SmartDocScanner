8b9bf0b52f63583e69fa8a016927ac97dd130df1
myyoutube7277-ai <myyoutube7277@gmail.com>
2026-09-07 04:49:36 +0530

    Update build.gradle.kts

--- SmartScanPro_GitHub_Build/app/build.gradle.kts
+++ SmartScanPro_GitHub_Build/app/build.gradle.kts
@@ -16,24 +16,35 @@ android {
         versionName = "1.0"
     }
 
-    buildFeatures { compose = true }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
+
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+
     implementation("androidx.navigation:navigation-compose:2.8.5")
+
     implementation("androidx.camera:camera-camera2:1.4.1")
     implementation("androidx.camera:camera-lifecycle:1.4.1")
     implementation("androidx.camera:camera-view:1.4.1")
+
     implementation("androidx.exifinterface:exifinterface:1.3.7")
+
     implementation("com.google.mlkit:text-recognition:16.0.1")
     implementation("com.google.mlkit:text-recognition-devanagari:16.0.1")
     implementation("com.google.mlkit:barcode-scanning:17.3.0")
+
     implementation("com.tom-roush:pdfbox-android:2.0.27.0")
 }
